d310052d2d55cee24662ab8f16d1e14460cdc3a8
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-08 23:59:55 +0530

    Refactor removeDuplicates function for clarity and remove duplicate definition

--- JS_DSA_Problems/Strings/index.js
+++ JS_DSA_Problems/Strings/index.js
@@ -86,3 +86,17 @@ function countVowelsAndConsonants(str) {
   }
   
   console.log(removeDuplicates("abbccc"));  
+  
+  function removeDuplicates(str) {
+    let result = '';
+    
+    for (let i = 0; i < str.length; i++) {
+      if (result.indexOf(str[i]) === -1) {
+        result += str[i];
+      }
+    }
+  
+    return result;
+  }
+  
+  console.log(removeDuplicates("abbccc"));  
