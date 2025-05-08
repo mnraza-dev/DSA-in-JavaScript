ee36ec68c6218f0e167e7c4e4e966ac3207ef725
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-08 23:59:05 +0530

    Add function to remove duplicate characters from a string

--- JS_DSA_Problems/Strings/index.js
+++ JS_DSA_Problems/Strings/index.js
@@ -69,3 +69,20 @@ function countVowelsAndConsonants(str) {
     return `Vowels: ${vowels}, Consonants: ${consonants}`;
   }
   
+
+
+  function removeDuplicates(str) {
+    let result = '';
+    let seen = new Set();
+  
+    for (let char of str) {
+      if (!seen.has(char)) {
+        result += char;
+        seen.add(char);
+      }
+    }
+    
+    return result;
+  }
+  
+  console.log(removeDuplicates("abbccc"));  
