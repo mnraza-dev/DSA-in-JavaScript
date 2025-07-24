56b7cfcbdf14214ecb83b687aed69185ed6c4302
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-24 21:29:02 +0530

    Question - 2: Sum of digits of a number

--- DSA_IN _JS/Basics/index.js
+++ DSA_IN _JS/Basics/index.js
@@ -11,3 +11,19 @@ function sumOfNaturalNumbers(n) {
   return sum;
 }
 console.log(sumOfNaturalNumbers(5));
+
+/**
+ * Question - 2: Sum of digits of a number
+ *  eg: sum of 145  = 10
+ */
+
+function sumOfDigits(num) {
+  let sum = 0;
+
+  while (num > 0) {
+    sum  += num % 10;
+    num = Math.floor(num / 10);
+  }
+  return sum;
+}
+console.log(sumOfDigits(145));
