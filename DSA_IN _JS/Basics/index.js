0ac1d41e804296a348366dc6470b41d2fc807e76
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-24 21:36:12 +0530

    Question - 3 : Count the numbers of digits of number

--- DSA_IN _JS/Basics/index.js
+++ DSA_IN _JS/Basics/index.js
@@ -21,9 +21,24 @@ function sumOfDigits(num) {
   let sum = 0;
 
   while (num > 0) {
-    sum  += num % 10;
+    sum += num % 10;
     num = Math.floor(num / 10);
   }
   return sum;
 }
 console.log(sumOfDigits(145));
+
+/**
+ * Question - 3 : Count the numbers of digits of number
+ *  eg: 3453  = 4
+ */
+
+function countNumberOfDigits(num) {
+  let count = 0;
+  do {
+    count++;
+    num = Math.floor(num / 10);
+  } while (num > 0);
+  return count;
+}
+console.log(countNumberOfDigits(12344255))
