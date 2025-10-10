72398b44df99abdd3ecd810538054a5eb317e486
Md Noorullah Raza <mnraza94@gmail.com>
2025-10-10 22:23:11 +0530

    strings in js
    Signed-off-by: Md Noorullah Raza <mnraza94@gmail.com>

---
+++ DSA_with_JS/Strings/index.js
@@ -0,0 +1,9 @@
+let name = "Noorullah";
+console.log(name)
+// 👉 You can think of a string as an array of characters:
+//    "Noor" → ['N', 'o', 'o', 'r']
+
+console.log(word[0]); // H
+console.log(word[4]); // o
+
+
