0bd3cf53702f2cf3e1fbdb91f4406616ee5da595
Md Noorullah Raza <mnraza94@gmail.com>
2025-10-10 22:23:50 +0530

    Count Vowels in a program
    Signed-off-by: Md Noorullah Raza <mnraza94@gmail.com>

--- DSA_with_JS/Strings/index.js
+++ DSA_with_JS/Strings/index.js
@@ -6,4 +6,18 @@ console.log(name)
 console.log(word[0]); // H
 console.log(word[4]); // o
 
+// Count Vowels
+function countVowels(str) {
+    let count = 0;
+    let vowels = "aeiouAEIOU";
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countVowels("Hello World")); // 3
+
 
