c82f5bf292abb312e3bbbb7561b960094ac51b32
Md Noorullah Raza <mnraza94@gmail.com>
2025-10-16 00:58:01 +0530

    var let & const

--- DSA_ULTIMATE/JS_BASICS/OPERATORS/index.js
+++ DSA_ULTIMATE/JS_BASICS/OPERATORS/index.js
@@ -36,3 +36,126 @@ console.log("x %= y",x);
 x **= y; // x = x ** y (Exponentiation assignment)
 
 console.log("x **= y",x);
+
+// Comparison Operators
+console.log(num1 == num2); // Equal to
+console.log(num1 != num2); // Not equal to
+console.log(num1 === num2); // Strict equal to
+console.log(num1 !== num2); // Strict not equal to
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+
+// Logical Operators
+/**
+ * Operands of all logical operators are always boolean if we do not pass boolean then js automatically 
+ * convert it into boolean
+ */
+
+// && (Logical AND)
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false       
+console.log("&& with non boolean", 5 && 0); // 0
+console.log("&& with non boolean", 5 && 6); // 6
+console.log("&& with non boolean", 0 && 6); // 0
+console.log("&& with non boolean", 0 && 0); // 0
+console.log("&& with non boolean", 5 && "Hello"); // "Hello"
+console.log("&& with non boolean", "Hello" && 5); // 5
+console.log("&& with non boolean", 0 && "Hello"); // 0
+console.log("&& with non boolean", "Hello" && 0); // 0
+console.log("&& with non boolean", null && "Hello"); // null
+console.log("&& with non boolean", "Hello" && null); // null
+console.log("&& with non boolean", undefined && "Hello"); // undefined
+console.log("&& with non boolean", "Hello" && undefined); // undefined
+console.log("&& with non boolean", NaN && "Hello"); // NaN
+console.log("&& with non boolean", "Hello" && NaN);
+console.log("&& with non boolean", NaN && 0); // NaN    
+console.log("&& with non boolean", NaN && NaN); // NaN
+console.log("&& with non boolean", "Hello" && "World"); // "World"
+console.log("&& with non boolean", "Hello" && 0); // 0
+console.log("&& with non boolean", "Hello" && null); // null
+console.log("&& with non boolean", "Hello" && undefined); // undefined
+console.log("&& with non boolean", "Hello" && NaN); // NaN  
+console.log("&& with non boolean", {} && 0); // 0
+console.log("&& with non boolean", [] && 0); // 0
+console.log("&& with non boolean", {} && []); // []
+console.log("&& with non boolean", [] && {}); // {}
+console.log("&& with non boolean", [] && null); // null
+console.log("&& with non boolean", null && []); // null
+console.log("&& with non boolean", {} && null); // null
+console.log("&& with non boolean", null && {}); // null
+console.log("&& with non boolean", {} && undefined); // undefined
+console.log("&& with non boolean", undefined && {}); // undefined
+console.log("&& with non boolean", [] && undefined); // undefined
+console.log("&& with non boolean", undefined && []);    // undefined
+console.log("&& with non boolean", {} && NaN); // NaN
+console.log("&& with non boolean", NaN && {}); // NaN
+console.log("&& with non boolean", [] && NaN); // NaN
+console.log("&& with non boolean", NaN && []); // NaN
+console.log("&& with non boolean", 5 && "Hello"); // "Hello"
+console.log("&& with non boolean", "Hello" && 5); // 5
+console.log("&& with non boolean", 0 && "Hello"); // 0
+console.log("&& with non boolean", "Hello" && 0); // 0
+console.log("&& with non boolean", null && "Hello"); // null
+console.log("&& with non boolean", "Hello" && null); // null
+console.log("&& with non boolean", undefined && "Hello"); // undefined
+console.log("&& with non boolean", "Hello" && undefined); // undefined
+console.log("&& with non boolean", NaN && "Hello"); // NaN
+console.log("&& with non boolean", "Hello" && NaN); // NaN  
+
+
+// || (Logical OR)
+
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+console.log("|| with non boolean", 5 || 0); // 5
+console.log("|| with non boolean", 0 || 6); // 6
+console.log("|| with non boolean", 5 || 6); // 5    
+console.log("|| with non boolean", 0 || ""); // ""
+console.log("|| with non boolean", "" || 7); // 7
+console.log("|| with non boolean", "" || null); // null
+console.log("|| with non boolean", null || 8); // 8
+console.log("|| with non boolean", undefined || 9); // 9
+console.log("|| with non boolean", undefined || null); // null
+console.log("|| with non boolean", NaN || "Hello"); // "Hello"
+console.log("|| with non boolean", NaN || 0); // 0
+console.log("|| with non boolean", NaN || NaN); // NaN
+console.log("|| with non boolean", "Hello" || "World"); // "Hello"
+console.log("|| with non boolean", "Hello" || 0); // "Hello"
+console.log("|| with non boolean", "Hello" || null); // "Hello"
+console.log("|| with non boolean", "Hello" || undefined); // "Hello"
+console.log("|| with non boolean", "Hello" || NaN); // "Hello"  
+console.log("|| with non boolean", {} || 0); // {}
+console.log("|| with non boolean", [] || 0); // []
+console.log("|| with non boolean", {} || []); // {}
+console.log("|| with non boolean", [] || {}); // []
+console.log("|| with non boolean", [] || null); // []
+console.log("|| with non boolean", null || []); // []   
+console.log("|| with non boolean", {} || null); // {}
+console.log("|| with non boolean", null || {}); // {}
+console.log("|| with non boolean", {} || undefined); // {}
+console.log("|| with non boolean", undefined || {}); // {}
+console.log("|| with non boolean", [] || undefined); // []
+console.log("|| with non boolean", undefined || []); // []
+console.log("|| with non boolean", {} || NaN); // {}
+console.log("|| with non boolean", NaN || {}); // {}
+console.log("|| with non boolean", [] || NaN); // []
+console.log("|| with non boolean", NaN || []); // []    
+console.log("|| with non boolean", 5 || "Hello"); // 5
+console.log("|| with non boolean", "Hello" || 5); // "Hello"
+console.log("|| with non boolean", 0 || "Hello"); // "Hello"
+console.log("|| with non boolean", "Hello" || 0); // "Hello"    
+console.log("|| with non boolean", null || "Hello"); // "Hello"
+console.log("|| with non boolean", "Hello" || null); // "Hello"
+console.log("|| with non boolean", undefined || "Hello");   // "Hello"
+console.log("|| with non boolean", "Hello" || undefined); // "Hello"
+
+//  ! (Logical NOT)
+
+console.log(!false); // true
+console.log(!true); // false  
---
+++ DSA_ULTIMATE/JS_BASICS/VARIABLES/index.js
@@ -0,0 +1,14 @@
+/**
+ * Variables in JavaScript
+ * 1. var
+ * 2. let
+ * 3. const
+ */
+
+// var : Function scoped
+// let and const : Block scoped
+
+var lname = "Raza";
+console.log(lname);
+lname = "Aly";
+console.log(lname);
