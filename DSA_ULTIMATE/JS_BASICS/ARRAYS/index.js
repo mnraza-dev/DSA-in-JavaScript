09a93841dd6167ac19254e4b242959149b56f57c
Md Noorullah Raza <mnraza94@gmail.com>
2025-10-17 00:04:14 +0530

    Basic functions in Arrays

--- DSA_ULTIMATE/JS_BASICS/ARRAYS/index.js
+++ DSA_ULTIMATE/JS_BASICS/ARRAYS/index.js
@@ -6,6 +6,25 @@
 // Arrays are special object in js, that can store one or more than one value of different types together.
 
 // 1. Creation of Arrays
+
 let fruits = ["Apple", "Banana", "Orange"];
 let numbers = [1, 2, 3, 4, 5];
 let mixed = [1, "Two", true, null, undefined, { name: "John" }, [1, 2, 3]];
+
+let vegetables = new Array("potato","tomato")
+let arr = new Array(4).fill(0);
+
+console.log(fruits[0]);
+console.log(fruits.length); // 3
+
+//  Basic functions
+
+fruits.push("Guava") // add a item to last
+fruits.pop() 
+
+fruits.unshift("Papaya")
+fruits.shift()
+
+console.log(fruits.indexOf("Apple"));
+
+
