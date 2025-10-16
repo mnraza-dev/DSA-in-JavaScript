4a2a784bdf91a2804decb1802c43529f638fd815
Md Noorullah Raza <mnraza94@gmail.com>
2025-10-17 00:36:43 +0530

    find the minimum value of given input values

--- DSA_ULTIMATE/PROBLEMS/index.js
+++ DSA_ULTIMATE/PROBLEMS/index.js
@@ -9,3 +9,20 @@ function isEvenOdd(num){
 // console.log(isEvenOdd(17));
 // console.log(isEvenOdd(18));
 // console.log(isEvenOdd(222));
+
+// Q.2 find the minimum value of given input values 
+
+function findMinimum(x, y,z) {
+    if(x < y && x < z){
+        return `${x} is minimum among all three values`
+    }
+    else if(y < x && y < z){
+        return `${y} is minimum among all three values`
+    }
+    else{
+        return `${z} is minimum among all three values`
+    }
+}
+// console.log(findMinimum(912,50,106));
+
+// Q.3 find the minimum value of given input values 
