3a445b6ab89134528cfc4adca109b552c416c257
Md Noorullah Raza <mnraza94@gmail.com>
2025-10-16 23:29:25 +0530

    Enhance string data type section with additional examples and common methods

--- DSA_ULTIMATE/JS_BASICS/DATA_TYPES/index.js
+++ DSA_ULTIMATE/JS_BASICS/DATA_TYPES/index.js
@@ -28,12 +28,38 @@
 
  // String
  // Strings can be defined using single quotes, double quotes, or backticks (template literals).
- 
+
  let str = "Hello, World!";
  let str2 = 'Hello, World!';
  let strObj = new String("Hello, World!"); // String object
  let str3 = `Hello, World!`; // Template literal    
  let str4 = `Hello, ${str}`; // Template literal with interpolation
-
+ let multiLineStr = `This is a
+multi-line
+string.`; // Multi-line string using template literals
+ let escapedStr = "He said, \"Hello!\""; // String with escaped quotes
+ let specialCharStr = "Line1\nLine2\tTabbed"; // String with special characters
+ console.log(strObj);
+ console.log(typeof strObj);
+ console.log(str2);
  console.log(str);
  console.log(typeof str); // string
+
+
+ // Concatenation
+    let greeting = "Hello";
+    let name = "Alice";
+    let message = greeting + ", " + name + "!"; // Using + operator
+    let message2 = `${greeting}, ${name}!`; // Using template literals
+    console.log(message);
+    console.log(message2);  
+
+    // Common String Methods
+    console.log(str.length); // Length of the string
+    console.log(str.toUpperCase()); // Convert to uppercase
+    console.log(str.toLowerCase()); // Convert to lowercase
+    console.log(str.indexOf("World")); // Find index of substring
+    console.log(str.slice(0, 5)); // Extract substring
+    console.log(str.replace("World", "JavaScript")); // Replace substring
+    console.log(str.split(", ")); // Split string into array
+    
